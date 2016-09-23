83420c8de66cc1d2919b4de980d5e6b19e9ec23c
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-09-23 14:54:28 -0400

    :sparkles: Adds animations to logo, borders, date, and location

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -43,4 +43,60 @@ window.onload = function(){
     this.setAttribute("placeholder", "NOTIFY ME WHEN APPLICATIONS OPEN");
     this.nextElementSibling.classList.add("hide");
   });
+
+  // Returns true if the specified element has been scrolled into the viewport.
+  function isElementInViewport(elem) {
+      var $elem = elem;
+
+      // Get the scroll position of the page.
+      var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+      var viewportTop = $(scrollElem).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      // Get the position of the element on the page.
+      var rect = $elem.getBoundingClientRect();
+      var elemTop = Math.round( rect.top + document.body.scrollTop );
+
+      var elemBottom = elemTop + $elem.offsetHeight;
+
+      return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+  }
+
+
+  function hasClass(el, cls) {
+    return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+  }
+
+  function addClass(ele,cls) {
+      if (!hasClass(ele,cls)) ele.className += " "+cls;
+  }
+
+  function removeClass(ele,cls) {
+      if (hasClass(ele,cls)) {
+          var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+          ele.className=ele.className.replace(reg,' ');
+      }
+  }
+
+  // Check if it's time to start the animation.
+  function checkAnimation(elem) {
+      var $elem = elem;
+
+      // If the animation has already been started
+      if (hasClass($elem, 'start')) return;
+
+      if (isElementInViewport($elem)) {
+          // Start the animation
+          addClass($elem, 'start');
+      }
+  }
+
+  // Capture scroll events
+  $(window).scroll(function(){
+      checkAnimation(document.getElementsByClassName('spartahack-title-animation')[0]);
+      checkAnimation(document.getElementById('event-date-animation'));
+      checkAnimation(document.getElementById('event-location-animation'));
+  });
+
+
 };
