b0a9c5bebac908860a608dd54ba463775ed04761
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-09-26 06:53:28 +0000

    :beetle: Potential 422 fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, except: :subscribe
+  protect_from_forgery with: :exception
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :subscribe
+
   include ApplicationHelper
   require 'json'
 
