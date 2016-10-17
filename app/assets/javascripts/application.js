f1b1ec4ac79bf67d5f9d373458e480bcd033b3fc
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-16 20:51:11 -0400

    :pencil: Adds more descriptive text for theme toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -73,7 +73,7 @@ $('.diamond, #logo-center').click(function() {
 
       swal({
         title:"Hey again",
-        text: "Want us to forget your theme preference?",
+        text: "Want us to forget your theme preference and toggle the theme?",
         showCancelButton: true,
         cancelButtonText: "Nah",
         confirmButtonColor: "#D4B166",
