a897808083380de74875ddecf89fdf44c9b296e6
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-23 13:16:17 -0400

    :package: Reworks how current_user is setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  include ApplicationHelper
   protect_from_forgery unless: -> { request.format.json? }
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,17 @@ class SessionsController < ApplicationController
   before_action :set_http_auth_token, only: [:create, :destroy]
   # Sign in
   def new
-
+    check_login
   end
 
   def create
     session_response = Session.create( { :email => session_params[:email], :password => session_params[:password] } )
     if session_response.errors.messages.empty?
       session[:current_session] = session_response
-      redirect_to root_url
+      redirect_to '/dashboard'
     else
       p session_response.errors.messages
+      flash[:error] = session_response.errors.messages[:base][0]
       render :new
     end
 
@@ -26,4 +27,11 @@ class SessionsController < ApplicationController
     def session_params
       params.require(:session).permit(:email, :password)
     end
+
+    def check_login
+      if current_user.present?
+        redirect_to '/dashboard'
+      end
+    end
+
 end
