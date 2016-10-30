5c18f8cb1885c1841c898ad0eb826723b6366934
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-30 21:16:05 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery with: :null_session
+  # protect_from_forgery unless: -> { request.format.json? }
 end
