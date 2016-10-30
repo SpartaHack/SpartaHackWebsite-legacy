f936bc271c0b8627a9c5996f026deccd425bd9ab
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-30 21:30:17 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
   include ApplicationHelper
-  protect_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery with: :null_session
+  # protect_from_forgery unless: -> { request.format.json? }
 end
