2b680f7147fa0765e28b7ef47c8e86ad31fc1427
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-11-02 09:09:52 -0400

    :sparkles: Conditionality

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -14,45 +14,8 @@ class ApplicationsController < ::ApplicationController
   end
 
   def create
-    flash[:params] = app_params
-    flash[:popup] = []
-    flash[:popup_errors] = []
-    if user_params['first_name'].blank? then flash[:popup].push("First name") end
-    if user_params['last_name'].blank? then flash[:popup].push("Last name") end
-    if user_params['email'].blank? then flash[:popup].push("Email") end
-    if user_params['email_confirmation'].blank? then flash[:popup].push("Email confirmation") end
-    if user_params['email_confirmation'] != user_params['email'] && user_params['email'].length > 1
-      flash[:popup_errors].push("Emails do not match.")
-    end
-    if user_params['password'].length < 7
-      flash[:popup_errors].push("Password must be greater than 5 characters")
-    end
-    if user_params['password'].blank? then flash[:popup].push("Password") end
-    if user_params['password_confirmation'].blank?
-      flash[:popup].push("Password confirmation")
-    end
-    if user_params['password_confirmation'] != user_params['password']
-      flash[:popup_errors].push("Passwords do not match.")
-    end
-    if app_params["birth_day"].blank? then flash[:popup].push("Birth day") end
-    if app_params["birth_month"].blank? then flash[:popup].push("Birth month") end
-    if app_params["birth_year"].blank? then flash[:popup].push("Birth year") end
-    if app_params["education"].blank? then flash[:popup].push("Current enrollment") end
-    if app_params["university"].blank? then flash[:popup].push("University") end
-    if app_params["travel_origin"].blank? then flash[:popup].push("Travel origin") end
-    if app_params["graduation_season"].blank? then flash[:popup].push("Graduation season") end
-    if app_params["graduation_year"].blank? then flash[:popup].push("Graduation year") end
-    if app_params['major'].blank? then flash[:popup].push("Major") end
-    if app_params['hackathons'].blank? then flash[:popup].push("Hackathons attended count") end
-    if params["mlh"] != "true"
-      flash[:popup_errors].push("You must agree to our terms.")
-    end
-    if flash[:popup].size > 0 || flash[:popup_errors].size > 0 || !flash[:popup_agree].blank?
-      flash[:app_params] = app_params.to_h
-      flash[:user_params] = user_params.to_h
-      redirect_to '/apply' and return
-    end
-
+    validation
+    conditionality
     user = User.new(user_params.to_h)
     if user.save
       session[:current_session] = Session.create( {
@@ -117,6 +80,47 @@ class ApplicationsController < ::ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def validation
+    flash[:params] = app_params
+    flash[:popup] = []
+    flash[:popup_errors] = []
+    if user_params['first_name'].blank? then flash[:popup].push("First name") end
+    if user_params['last_name'].blank? then flash[:popup].push("Last name") end
+    if user_params['email'].blank? then flash[:popup].push("Email") end
+    if user_params['email_confirmation'].blank? then flash[:popup].push("Email confirmation") end
+    if user_params['email_confirmation'] != user_params['email'] && user_params['email'].length > 1
+      flash[:popup_errors].push("Emails do not match.")
+    end
+    if user_params['password'].length < 7
+      flash[:popup_errors].push("Password must be greater than 5 characters")
+    end
+    if user_params['password'].blank? then flash[:popup].push("Password") end
+    if user_params['password_confirmation'].blank?
+      flash[:popup].push("Password confirmation")
+    end
+    if user_params['password_confirmation'] != user_params['password']
+      flash[:popup_errors].push("Passwords do not match.")
+    end
+    if app_params["birth_day"].blank? then flash[:popup].push("Birth day") end
+    if app_params["birth_month"].blank? then flash[:popup].push("Birth month") end
+    if app_params["birth_year"].blank? then flash[:popup].push("Birth year") end
+    if app_params["education"].blank? then flash[:popup].push("Current enrollment") end
+    if app_params["university"].blank? then flash[:popup].push("University") end
+    if app_params["travel_origin"].blank? then flash[:popup].push("Travel origin") end
+    if app_params["graduation_season"].blank? then flash[:popup].push("Graduation season") end
+    if app_params["graduation_year"].blank? then flash[:popup].push("Graduation year") end
+    if app_params['major'].blank? then flash[:popup].push("Major") end
+    if app_params['hackathons'].blank? then flash[:popup].push("Hackathons attended count") end
+    if params["mlh"] != "true"
+      flash[:popup_errors].push("You must agree to our terms.")
+    end
+    if flash[:popup].size > 0 || flash[:popup_errors].size > 0
+      flash[:app_params] = app_params.to_h
+      flash[:user_params] = user_params.to_h
+      redirect_to '/apply' and return
+    end
   end
 
   def conditionality
