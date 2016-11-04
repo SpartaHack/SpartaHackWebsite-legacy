c58094f597ea07cce00b83d1aeb4aaae57dbab44
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-11-03 20:21:51 -0400

    :beetle:

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -21,6 +21,8 @@ class ApplicationsController < ::ApplicationController
     @user = User.new(user_params.to_h)
     begin
       if @user.save
+        session.clear
+        reset_session
         session[:current_session] = @user.id
         # set_user_auth_token
         ActiveResource::Base.headers["X-WWW-User-Token"] = "#{ @user.auth_token }"
