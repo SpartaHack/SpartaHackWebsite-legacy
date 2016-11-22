1b8abc1cfe5535a648d78f49f8777c8d74e8a73e
Joshua Miles <milesjos@msu.edu>
2016-11-21 22:16:14 -0500

    :sparkles: Add RSVP button on the dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@
 var mobileToggled = false
 var headerBoxShadow = "0px 0px 22px 0px rgba(0,0,0,0.04)";
 var themeElements = "body, nav, .active-q, .sweet-alert, input, #popup-wrapper, \
-#popup-error-wrapper, .popup, #statement_count, #dashboard #app, #mlh-trust-badge";
+#popup-error-wrapper, .popup, #statement_count, #dashboard #app, #mlh-trust-badge, .action-button";
 
 ////////////////////////////////////////////////////
 // Dark Theme
