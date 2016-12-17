c333ba4513e67177fd7ebc81f31428ba7596ff8e
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-12-17 00:20:01 -0500

    :sparkles: Request password reset link page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,46 @@ class SessionsController < ApplicationController
 
   end
 
+  def forgot_password
+  end
+
+  def send_password_email
+    unless session_params[:email].present?
+      flash[:error] = "Email is required"
+      render :forgot_password and return
+    end
+
+    begin
+      url = URI.parse("#{ENV['API_SITE']}/users/request_password_token")
+      req = Net::HTTP::Post.new(url.to_s)
+      req.set_form_data({'email' => session_params[:email]}, ';')
+      req.add_field("Authorization", "Token token=\"#{ENV['API_AUTH_TOKEN']}\"")
+
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl = true if url.scheme == 'https'
+      res = res.start {|http|
+        http.request(req)
+      }
+
+      user =  JSON.parse(res.body)
+      unless user['errors'].blank?
+        flash[:error] = "No account with email: <strong>#{session_params[:email]}</strong>".html_safe
+        render :forgot_password and return
+      end
+
+      UserMailer.reset_password_email(
+        user['first_name'], user['email'], user['reset_password_token']
+      ).deliver_now
+
+      flash[:error] = "Reset link sent to <strong>#{session_params[:email]}</strong>".html_safe
+    rescue => e
+      p e
+      flash[:error] = "Error getting Reset Token"
+    end
+
+    render :forgot_password and return
+  end
+
   def destroy
     User.current_user = nil
     session.delete(:current_session)
